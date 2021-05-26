e2191d310b8d3390aba88615dece5cef2b904281
Srikar <srikarsamanchi1999@gmail.com>
2021-05-26 22:50:50 +0530

    Added: flipcoin

---
+++ flipCoin.java
@@ -0,0 +1,22 @@
+public class flipCoin{
+public static void main(String args[]){
+int head=0;
+int tail=0; 
+int n=10;
+for(int i=1;i<=n;i++)
+{double coin=Math.random();
+if(coin<0.5){
+head++;
+}else{
+tail++;
+}
+}
+int percenthead=(head*100)/10;
+int percenttail=(head*100)/10;
+System.out.println("Head Count = " +head);
+System.out.println("Tail Count = " +tail);
+System.out.println("Head Percentage" +percenthead);
+System.out.println("Tail percentage" +percenttail);
+        
+}
+}
