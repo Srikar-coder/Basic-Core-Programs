4caf008f3cbcee5002fbcc17d5f787dd39e05283
Srikar <srikarsamanchi1999@gmail.com>
2021-05-27 00:03:28 +0530

    Added : swapping of two numbers

---
+++ swapOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class swapOfNumbers{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter two values");
+int a=sc.nextInt();
+int b=sc.nextInt();
+
+System.out.println("a = "+a+" b = "+b);
+a=a+b;
+b=a-b;
+a=a-b;
+
+System.out.println("a = "+a+" b = "+b);
+}
+
+}
